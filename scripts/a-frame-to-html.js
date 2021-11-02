22ec2a2f60c28abe25a9de53be083ec408cabf81
Vojtěch Leischner <50262885+trackme518@users.noreply.github.com>
2021-11-02 18:37:20 +0100

    up

---
+++ scripts/a-frame-to-html.js
@@ -0,0 +1,57 @@
+//interact between  A-frame and rest of HTML DOM
+AFRAME.registerComponent('a-frame-to-html', {
+   schema: {
+    id: {type: 'number', default: 0},
+    fetchurl: {default: 'http://yourhomepage.com'},
+    target: {type: 'selector'}
+  },
+  init: function () {
+    var target = this.data.target;
+    var postid = this.data.id;
+    var fetchurl = this.data.fetchurl;
+    //var currpath = this.data.path;
+   // var type = this.data.type;
+    var el = this.el;
+    
+    this.el.addEventListener('click', showcontent ); //add event listener for mouse click
+    
+    function showcontent(){
+            
+       target.classList.remove("down");
+       target.classList.add("show");
+
+      var fetchQuery = fetchurl+'/wp-json/wp/v2/posts/'+postid;
+      console.log("fetch query: "+fetchQuery);
+      //https://tricktheear.eu/wp-admin/customize.php?theme=VRview&return=https%3A%2F%2Ftricktheear.eu%2Fwp-admin%2Fthemes.php
+
+       
+       jQuery.get( fetchQuery, function( data ) {
+         var htmltxt = '<div id="innerhtml">';
+         
+         htmltxt += '<h1>'+data.title.rendered+'</h1>';
+         
+         htmltxt += data.content.rendered; //append actual post from wp db
+         //htmltxt += '<div class="overlay"><a id="btn-close" onclick="javascript:hideme(\''+target.id+'\')">×</a></div>'; //×
+          htmltxt += '<div class="overlay"><a id="btn-close" onclick="javascript:hideme(\''+target.id+'\')">&nbsp;BACK <i class="fas fa-arrow-circle-left"></i></a></div>'; //×
+         htmltxt += '</div>';
+         target.innerHTML = htmltxt; //target modal overlay with ijected content
+        
+        //hide post when clicked outside post area - added new fce - based on user feedback
+        window.addEventListener('click', function(e){	
+      	if (document.getElementById('innerhtml').contains(e.target)){
+        	//alert("Clicked in Box");
+        } else{
+           hideme(target.id);
+            //alert("Clicked outside Box");
+          }
+        })
+        
+        
+        
+        });
+        //<i class="fas fa-arrow-circle-left"></i>
+        ///adjustCloseButton(); //call function that mimics the fixed position property but changes the top offset based on height of nav menu
+    } 
+ 
+  }
+});
